1a13d887255d689d6e8cf54062f2c0c934500f89
Sam Lee <sam.lee03@myhunter.cuny.edu>
2024-07-25 22:54:51 -0400

    add navbar, start search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from './components/Navbar'
+import Search from './components/Search'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Navbar/>
+      <Search/>
     </>
   )
 }
