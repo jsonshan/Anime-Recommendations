9fd20ab61be1d906c094a431c222257a71c429f7
Sam Lee <sam.lee03@myhunter.cuny.edu>
2024-07-27 22:10:57 -0400

    add search tab

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "../styles/Search.css"
+import Cross from "../assets/cross.svg"
+
 function Search() {
+  const containerRef = useRef(null);
+  const handleFocus = () => {
+    console.log("Running");
+    if (containerRef.current) {
+      containerRef.current.style.setProperty('--after-background-color', '#F47521');
+    }
+  };
+  const handleBlur = () => {
+    if (containerRef.current) {
+      containerRef.current.style.setProperty('--after-background-color', '#59595B'); // Change to black when blurred
+    }
+  };
+
   return (
     <>
         <div className="container">
-            <input className="search-bar" type="text" placeholder="Search..."></input>
+          <div className="search-container"
+          ref={containerRef}
+          >
+            <input className="search-bar" type="text" placeholder="Search..."
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            ></input>
+            <button>
+              <img className="cross invert" src={Cross}/>
+
+            </button>
+          </div>
+        </div>
+        <div className="results-tab">
+
         </div>
     </>
     
