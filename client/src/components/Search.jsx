862b8a4a821024532778514336f29ab86d078f67
Sam Lee <sam.lee03@myhunter.cuny.edu>
2024-07-29 22:05:02 -0400

    add card components

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import "../styles/Search.css"
+import Card from './Card'
 import Cross from "../assets/cross.svg"
 import CrunchyMascot from "../assets/crunchyroll-mascot.png"
 
@@ -18,6 +19,40 @@ function Search() {
       containerRef.current.style.setProperty('--after-background-color', '#59595B'); // Change to black when blurred
     }
   };
+
+  // Check for Typing
+  const [isTypingStopped, setIsTypingStopped] = useState(false);
+
+  // Typing delay time in milliseconds
+  const typingDelay = 400; // 1 second
+
+  // Timer for tracking typing
+  let typingTimer;
+
+  // Function to be called when typing stops
+  const handleTypingStopped = () => {
+    setIsTypingStopped(true);
+    console.log('User stopped typing!');
+  };
+
+  // Effect hook to handle the typing timer
+  useEffect(() => {
+    if (inputText) {
+      // User is typing, reset the typing stopped state
+      setIsTypingStopped(false);
+
+      // Clear previous timer
+      clearTimeout(typingTimer);
+
+      // Set a new timer
+      typingTimer = setTimeout(handleTypingStopped, typingDelay);
+
+      // Cleanup the timer on component unmount or inputValue change
+      return () => clearTimeout(typingTimer);
+    }
+  }, [inputText]);
+
+
   const handleInputText = (event) => {
     // event.target.value == '' ? setHasResult(false) ? setHasResult(true);
     if (event.target.value != 'no-result'){ // CHECK WHETHER THERE ARE RESULTS OR NOT
@@ -27,6 +62,10 @@ function Search() {
     }
     setInputText(event.target.value);
   }
+
+  // useEffect(() => {
+  //   if (isTypingStopped)
+  // }, [isTypingStopped])
 
   return (
     <>
@@ -45,16 +84,16 @@ function Search() {
             </button>
           </div>
         </div>
-        {inputText != "" 
+        {inputText && isTypingStopped != "" 
           ?
             (hasResult
               ? 
                 <div className="black-wrapper">
                   <div className="result-content-wrapper">
                     <h4>Top Results:</h4>
-                    <div>
-                      <img src="https://dummyimage.com/100x150/dadada/fff"/>
-
+                    <div className="anime-container">
+                      <Card title="Demon Slayer: Kimetsu no Yaiba" rating="5.0" imgSource="https://m.media-amazon.com/images/I/81FqK8mQYiL._SL1500_.jpg"/>
+                      <Card title="Jujutsu Kaisen" rating="4.7" imgSource="https://cdn.animenewsnetwork.com/hotlink/thumbnails/max700x700/cms/news.6/196392/jjk_visual.jpg"/>
                     </div>
                   </div>
                 </div>
